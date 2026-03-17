cff24218637141fb530fe625506b61a1ffee1520
Camilo Cortes Parra <ccortesp@ific.uv.es>
2026-03-17 13:20:41 +0100

    Not needed anymore

--- notebook.sh
+++
@@ -1,4 +0,0 @@
-# Starting the Jupyter notebook server
-# Usage: . notebook.sh
-
-( jupyter notebook --no-browser --port=9000 & echo $! > /tmp/jupyter_pid ) && trap "kill $(cat /tmp/jupyter_pid)" EXIT
